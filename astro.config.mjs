096a33c1595b0ec97bf6b2fd813cf9fb99180e27
liulipeng123 <2892667683@qq.com>
2025-09-06 20:24:42 +0800

    deploy giyhub pages

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  base: '/cv/', // 替换为你的仓库名称
+  site: 'https://liulipeng123.github.io/cv/', // 替换为你的 GitHub 用户名和仓库名称
+});
+
